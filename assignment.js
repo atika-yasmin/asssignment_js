1a6b4a069a0fbea5e56e0f8e0b64f19a65af9aad
atika-yasmin <atika.yasmin.19@gmail.com>
2020-07-20 10:07:51 +0600

    finished

--- assignment.js
+++ assignment.js
@@ -15,6 +15,32 @@ function woodCalculator(chair, table, bed){
 var result = woodCalculator(6, 2, 3);
 console.log("Total wood = ", result);
 
+//brickCalculator
+function brickCalculator(floor){
+    var brick = 0;
+    if(floor <= 10){
+        brick = floor * 15 * 1000;
+    }
+    else if (floor <= 20) {
+        var firstpart = 10 * 15 * 1000;
+        var remain = floor - 10;
+        var secondpart = remain * 12 * 1000;
+        brick = firstpart + secondpart;
+    }
+    else{
+        var firstpart = 10 * 15 * 1000;
+        var secondpart = 10 * 12 * 1000;
+        var remain = floor - 20;
+        var thirdpart = remain * 10 * 1000;
+        brick = firstpart + secondpart + thirdpart;
+    }
+    return brick;
+}
+var result = brickCalculator(12);
+console.log("Total brick = ", result);
+
+
+
 
 //tinyFriend
 function tinyFriend(name){
